9fc4fab9d05db6b4d3a58fde1e3aa7fa5204b739
yld <1799513674@qq.com>
2022-06-20 17:00:23 +0800

    new

---
+++ testdir/test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"reflect"
+	"strconv"
+)
+
+func Any(value interface{}) string {
+	return formatAtom(reflect.ValueOf(value))
+}
+
+func formatAtom(v reflect.Value) string {
+	switch v.Kind() {
+	case reflect.Invalid:
+		return "invalid"
+	case reflect.Int, reflect.Int32:
+		return v.String()
+	default:
+		return "Hello, world"
+	}
+}
