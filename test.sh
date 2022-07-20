4949e8a88cb7e7b5c8293d4337593f3e33c4d63b
yld <1799513674@qq.com>
2022-07-20 20:34:11 +0800

    yld test

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-go import test.go > testtmp.go
-cat testtmp.go > test.go
-rm testtmp.go
-
-go run test.go
--- yld_test.go
+++ yld_test.go
@@ -1,9 +1,35 @@
 package main
 
-import "fmt"
 import "testing"
+import "sync"
 
-func TestMyTest(t *testing.T) {
-	fmt.Println("Hello, world\nHi")
-	t.Fatalf("Nooooooooooooooooooooooooooo")
+func TestParrelism(t *testing.T) {
+	race := 0
+
+	ch := make(chan interface{})
+	// receiver
+	go func() {
+		for i := 0; i < 2000000; i++ {
+			<-ch
+		}
+	}()
+
+	// two sender
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	sender := func() {
+		for i := 0; i < 1000000; i++ {
+			ch <- nil
+			race++
+		}
+		wg.Done()
+	}
+	go sender()
+	go sender()
+
+	wg.Wait()
+
+	if race != 2000000 {
+		t.Fatalf("race = %d, should be 2000000", race)
+	}
 }
